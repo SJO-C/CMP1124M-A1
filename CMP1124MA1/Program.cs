9b99c24f71f078d07878553ab5133d3a944e7222
Samuel Jonathan Orman-Chan <samuelorman.chan@outlook.com>
2022-04-20 13:30:09 +0100

    Started on Recasting Strings as Ints
    Signed-off-by: Samuel Jonathan Orman-Chan <samuelorman.chan@outlook.com>

---
+++ CMP1124MA1/Program.cs
@@ -0,0 +1,39 @@
+﻿// See https://aka.ms/new-console-template for more information
+class Program
+{
+    public Int32[] Recast2Ints (List<String> sharesUSortedstr)
+    {
+        Int32[] result = new Int32[sharesUSortedstr.Count];
+        int tempCounter = 0;
+        foreach (String i in sharesUSortedstr)
+        {
+            
+            result[tempCounter] = Int32.Parse(i);
+            tempCounter++;
+        }
+    }
+    
+    static void Main()
+    {
+        Console.WriteLine("Shares_1_256");
+         String[] strShare1256 = System.IO.File.ReadAllLines(@"C:\Users\samue\OneDrive - University of Lincoln\Lectures\AlgoComplex-Y1\Assess-1\Share_1_256.txt");
+        foreach (String str in strShare1256)
+        {
+            Console.WriteLine(str);
+        }
+        Console.WriteLine("Shares_2_256");
+        String[] strShare2256 = System.IO.File.ReadAllLines(@"C:\Users\samue\OneDrive - University of Lincoln\Lectures\AlgoComplex-Y1\Assess-1\Share_2_256.txt");
+        foreach (String str in strShare1256)
+        {
+            Console.WriteLine(str);
+        }
+        Console.WriteLine("Shares_3_256");
+        String[] strShare3256 = System.IO.File.ReadAllLines(@"C:\Users\samue\OneDrive - University of Lincoln\Lectures\AlgoComplex-Y1\Assess-1\Share_3_256.txt");
+        foreach (String str in strShare1256)
+        {
+            Console.WriteLine(str);
+        }
+        Console.WriteLine();
+        Console.ReadKey();
+    }
+}
