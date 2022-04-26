4ca98f3fac174786ca373915ee212e992afe810f
Samuel Jonathan Orman-Chan <samuelorman.chan@outlook.com>
2022-04-26 16:34:43 +0100

    Search Sort of Works but not outputting positons.

---
+++ TP1_digitExtract/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+List<int> validCode = new();
+int num1 = 147981;
+int digit = new();
+while (num1 >= 1)
+{
+    digit = num1 % 10;
+    validCode.Add(digit);
+    num1 = num1 / 10;
+}
+
+
+foreach(int i in validCode)
+{
+    Console.Write(i + ",");
+}
